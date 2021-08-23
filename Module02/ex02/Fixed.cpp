e6f899ad69aff9b69e0065d71f2329e6f156132b
bulat <b.barzhanov@yandex.ru>
2021-08-23 11:23:44 +0300

    add make for module 02

--- Module02/ex02/Fixed.cpp
+++ Module02/ex02/Fixed.cpp
@@ -1,37 +1,37 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed() : _fixed_point(0) {
-	std::cout<<GREEN<<"Default constructor called"<<RESET<<std::endl;
+//	std::cout<<GREEN<<"Default constructor called"<<RESET<<std::endl;
 }
 
 Fixed::Fixed( const int value ) : _fixed_point(value << Fixed::_fractional_bits){
-	std::cout<<GREEN<<"Int constructor called"<<RESET<<std::endl;
+//	std::cout<<GREEN<<"Int constructor called"<<RESET<<std::endl;
 }
 
 Fixed::Fixed( const float value ) :
 _fixed_point(roundf(value * (1 << Fixed::_fractional_bits))){
-	std::cout<<GREEN<<"Float constructor called"<<RESET<<std::endl;
+//	std::cout<<GREEN<<"Float constructor called"<<RESET<<std::endl;
 }
 
 Fixed::Fixed( Fixed const & src){
-	std::cout<<BLUE<<"Copy constructor called"<<RESET<<std::endl;
+//	std::cout<<BLUE<<"Copy constructor called"<<RESET<<std::endl;
 	*this = src;
 }
 
 Fixed::~Fixed(){
-	std::cout<<RED<<"Destructor called"<<RESET<<std::endl;
+//	std::cout<<RED<<"Destructor called"<<RESET<<std::endl;
 }
 
 
 
 
 int			Fixed::getRawBits( void ) const { 
-	std::cout<<BROWN<<"getRawBits member function called"<<RESET<<std::endl;
+//	std::cout<<BROWN<<"getRawBits member function called"<<RESET<<std::endl;
 	return (this->_fixed_point);
 }
 
 void		Fixed::setRawBits ( int const raw ) { 
-	std::cout<<RANDOM<<"setRawBits member function called"<<RESET<<std::endl;
+//	std::cout<<RANDOM<<"setRawBits member function called"<<RESET<<std::endl;
 	_fixed_point = raw << _fractional_bits;
 }
 
@@ -52,7 +52,7 @@ std::ostream &	operator << (std::ostream & o, Fixed const & input){
 }
 
 Fixed &	Fixed::operator = (Fixed const & value){
-	std::cout<<RANDOM<<"Assignation operator called"<<RESET<<std::endl;
+//	std::cout<<RANDOM<<"Assignation operator called"<<RESET<<std::endl;
 	if (this != &value)
 		_fixed_point = value.getRawBits();
 	return (*this);
